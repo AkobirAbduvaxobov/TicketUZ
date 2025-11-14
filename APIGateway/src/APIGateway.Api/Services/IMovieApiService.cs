aed8e169cc7b1321a9ad21eec9007206c9d21db2
Akobir <akobirabduvahobov@gmail.com>
2025-11-14 17:02:34 +0500

    Added

--- APIGateway/src/APIGateway.Api/Services/IMovieApiService.cs
+++ APIGateway/src/APIGateway.Api/Services/IMovieApiService.cs
@@ -1,6 +1,30 @@
-﻿namespace APIGateway.Api.Services
+﻿using APIGateway.Api.Dtos.MovieDtos;
+
+namespace APIGateway.Api.Services;
+
+public interface IMovieApiService
 {
-    public interface IMovieApiService
-    {
-    }
+    Task<long> AddCinemaHallAsync(CinemaHallCreateDto cinemaHallCreateDto);
+    Task UpdateCinemaHallAsync(CinemaHallUpdateDto cinemaHallUpdateDto);
+    Task DeleteCinemaHallAsync(long cinemaHallId);
+    Task<CinemaHallDto> GetCinemaHallByIdAsync(long cinemaHallId);
+    Task<List<CinemaHallDto>> GetAllCinemaHallsAsync();
+
+
+    Task<long> AddMovieAsync(MovieCreateDto movieCreateDto);
+    Task UpdateMovieAsync(MovieUpdateDto movieUpdateDto);
+    Task<MovieDto> GetMovieByIdAsync(long movieId);
+    Task<List<MovieDto>> GetAllMoviesAsync();
+    Task DeleteMovieAsync(long movieId);
+
+    Task<SeatDto> GetSeatByIdAsync(long seatId);
+
+
+    Task<long> AddShowtimeAsync(ShowtimeCreateDto showtimeCreateDto);
+    Task DeleteShowTimeByIdAsync(long showtimeId);
+    Task<List<ShowtimeDto>> GetShowtimesAsync();
+    Task<ShowtimeDto> GetShowtimeByIdAsync(long showtimeId);
+    Task MakeShowtimeAvailableAsync(long showtimeId);
+    Task UpdateShowtimeAsync(ShowtimeUpdateDto showtimeUpdateDto);
+
 }
--- APIGateway/src/APIGateway.Api/Services/MovieApiService.cs
+++ APIGateway/src/APIGateway.Api/Services/MovieApiService.cs
@@ -1,6 +1,91 @@
-﻿namespace APIGateway.Api.Services
+﻿using APIGateway.Api.Dtos.MovieDtos;
+
+namespace APIGateway.Api.Services;
+
+public class MovieApiService : IMovieApiService
 {
-    public class MovieApiService
+    public Task<long> AddCinemaHallAsync(CinemaHallCreateDto cinemaHallCreateDto)
     {
+        throw new NotImplementedException();
+    }
+
+    public Task<long> AddMovieAsync(MovieCreateDto movieCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<long> AddShowtimeAsync(ShowtimeCreateDto showtimeCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteCinemaHallAsync(long cinemaHallId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteMovieAsync(long movieId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteShowTimeByIdAsync(long showtimeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<CinemaHallDto>> GetAllCinemaHallsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<MovieDto>> GetAllMoviesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CinemaHallDto> GetCinemaHallByIdAsync(long cinemaHallId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MovieDto> GetMovieByIdAsync(long movieId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SeatDto> GetSeatByIdAsync(long seatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ShowtimeDto> GetShowtimeByIdAsync(long showtimeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ShowtimeDto>> GetShowtimesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task MakeShowtimeAvailableAsync(long showtimeId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCinemaHallAsync(CinemaHallUpdateDto cinemaHallUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateMovieAsync(MovieUpdateDto movieUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateShowtimeAsync(ShowtimeUpdateDto showtimeUpdateDto)
+    {
+        throw new NotImplementedException();
     }
 }
