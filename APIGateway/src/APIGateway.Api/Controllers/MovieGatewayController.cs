19dd2ebd28fd9ab0e44818d66454f3fca2014a47
Temurbek Mansurov <Diluc-Kirara>
2025-11-16 14:52:54 +0500

    addsone

--- APIGateway/src/APIGateway.Api/Controllers/MovieGatewayController.cs
+++ APIGateway/src/APIGateway.Api/Controllers/MovieGatewayController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using APIGateway.Api.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieSystem.Api.Dtos;
 
 namespace APIGateway.Api.Controllers
 {
@@ -7,5 +9,25 @@ namespace APIGateway.Api.Controllers
     [ApiController]
     public class MovieGatewayController : ControllerBase
     {
+        private readonly IMovieApiService _movieApiService;
+
+        public MovieGatewayController(IMovieApiService movieApiService)
+        {
+            _movieApiService = movieApiService;
+        }
+
+        [HttpGet("/CinemaHall")]
+        public async Task<List<CinemaHallDto>> GetCinemaHalls()
+        {
+            var cinemaHalls = await _movieApiService.GetAllCinemaHallsAsync();
+            return cinemaHalls;
+        }
+
+        [HttpGet("/Movies")]
+        public async Task<List<MovieDto>> GetMovies()
+        {
+            var movies = await _movieApiService.GetAllMoviesAsync();
+            return movies;
+        }
     }
 }
